446de9b5bbb5cc841b733c73b429cb52e00cc098
Giorgos Giagas <giorgosg@gmail.com>
2025-02-07 20:26:51 +0200

    added ScaledTransform to exports

--- src/box2d/__init__.py
+++ src/box2d/__init__.py
@@ -1,5 +1,5 @@
 from .world import World
-from .math import Vec2, Rot, Transform, AABB, Mat22
+from .math import Vec2, Rot, Transform, AABB, Mat22, ScaledTransform
 from .body import Body
 from .shape import Box, Circle, Polygon
 from .joint import MouseJoint
