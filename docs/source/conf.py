4c8c0493b05afb8dce3413da579a6b6dc260880b
Giorgos Giagas <giorgosg@gmail.com>
2025-02-10 11:50:25 +0200

    add pre-commit hook for black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,21 +8,22 @@ import sys, os, sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'box2d-py'
-copyright = '2025, Giorgos Giagas'
-author = 'Giorgos Giagas'
-release = '2025'
+project = "box2d-py"
+copyright = "2025, Giorgos Giagas"
+author = "Giorgos Giagas"
+release = "2025"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode',
-              'sphinx_rtd_theme',
-              ]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # Mock CFFI imports that require compiled binaries
@@ -35,6 +36,6 @@ napoleon_include_init_with_doc = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-sys.path.insert(0, os.path.abspath('../../src/'))  # Add project to path
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+sys.path.insert(0, os.path.abspath("../../src/"))  # Add project to path
